7c309580ef124e1fb016343d166329b41d37dbc4
Merci Jacob <mercihabam@gmail.com>
2023-08-17 17:15:16 +0200

    assertion with getByRole query

---
+++ src/components/application/Application.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react"
+import { Application } from "./Application"
+
+describe("Application", () =>{
+    test("renders correctly", () => {
+        render(<Application />);
+
+        const pageHeading = screen.getByRole('heading', {
+            level: 1
+        });
+        expect(pageHeading).toBeInTheDocument();
+
+        const sectionHeading = screen.getByRole("heading", {
+            level: 2
+        })
+        
+        const nameElement = screen.getByRole("textbox", {
+            name: "Name"
+        });
+        expect(nameElement).toBeInTheDocument();
+
+        const bioElement = screen.getByRole('textbox', {
+            name: "Bio"
+        });
+        expect(bioElement).toBeInTheDocument()
+
+        const jobLocationElement = screen.getByRole('combobox');
+        expect(jobLocationElement).toBeInTheDocument();
+    })
+})
