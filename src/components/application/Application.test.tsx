9c68ab885ffd5542f38f9faf007d61a1c9d89a67
Merci Jacob <mercihabam@gmail.com>
2023-08-17 18:01:26 +0200

    assertion with getByAltText

--- src/components/application/Application.test.tsx
+++ src/components/application/Application.test.tsx
@@ -16,6 +16,9 @@ describe("Application", () =>{
 
         const paragraphElement = screen.getByText("All fields are mandatory");
         expect(paragraphElement).toBeInTheDocument();
+
+        const imageElement = screen.getByAltText("a person with a laptop");
+        expect(imageElement).toBeInTheDocument();
         
         const nameElement = screen.getByDisplayValue("Jacob");
         expect(nameElement).toBeInTheDocument();
